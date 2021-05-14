53b40546964836da50a27b9cf7865ccd16f7c0ab
Jason Meurer <30842226+jasonmeurer@users.noreply.github.com>
2021-05-14 11:37:37 -0400

    showheaders update

--- webstage.sh
+++ webstage.sh
@@ -2,5 +2,5 @@
 apt-get update
 apt-get install -y apache2 php7.2 libapache2-mod-php7.2
 rm -f /var/www/html/index.html
-wget -O /var/www/html/index.php https://raw.githubusercontent.com/jasonmeurer/azure-appgw-stdv2/master/showheaders.php
+wget -O /var/www/html/index.php https://raw.githubusercontent.com/jasonmeurer/showheaders/master/showheaders.php
 systemctl restart apache2
